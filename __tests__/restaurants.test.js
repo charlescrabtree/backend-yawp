10d4343ee1ee317813a8597a5d1150275e4820e7
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-11-29 16:48:39 -0800

    added totallyRealUser and registerAndLogin to restaurants.test to create a user that can add reviews

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -2,6 +2,14 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const totallyRealUser = {
+  firstName: 'Lily',
+  lastName: 'Dogwalker',
+  email: 'aaaa@aaaa.com',
+  password: 'hashishi9871',
+};
 
 describe('/api/v1/restaurants routes', () => {
   beforeEach(() => {
@@ -81,36 +89,17 @@ describe('/api/v1/restaurants routes', () => {
         "website": "http://www.muccaosteria.com",
       }
     `);
-    // Object {
-    //     "cost": 3,
-    //     "cuisine": "Italian",
-    //     "id": "2",
-    //     "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
-    //     "name": "Mucca Osteria",
-    //     "reviews": Array [
-    //         Object {
-    //           "detail": "Best restaurant ever!",
-    //           "id": "1",
-    //           "stars": 5,
-    //           "user_id": "1",
-    //         },
-    //         Object {
-    //           "detail": "Terrible service :(",
-    //           "id": "2",
-    //           "stars" 1,
-    //           "user_id": "2",
-    //         },
-    //         Object {
-    //           "detail": "It was fine.",
-    //           "id": "3",
-    //           "stars": 4,
-    //           "user_id": "3",
-    //         },
-    //     ],
-    //     "website": "http://www.muccaosteria.com",
-    // }
   });
+  
+  const registerAndLogin = async (userProps = {}) => {
+    const password = userProps.password ?? totallyRealUser.password;
+    const agent = request.agent(app);
+    const user = await UserService.create({ ...totallyRealUser, ...userProps });
 
+    const { email } = user;
+    await (await agent.post('/api/v1/users/sessions')).setEncoding({ email, password });
+    return [agent, user];
+  };
   afterAll(() => {
     pool.end();
   });
