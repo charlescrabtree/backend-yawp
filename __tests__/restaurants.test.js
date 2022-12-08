7549c5f19c66e70272b49f1cce6262625d0a9f0e
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-12-08 11:05:30 -0800

    added AuthDelete

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -138,7 +138,7 @@ describe('/api/v1/restaurants routes', () => {
     const res = await agent.delete(`/api/v1/reviews/${review.body.id}`);
     expect(res.status).toBe(200);
 
-    const getResp = await request(app).get(`/api/v1/reviews/${review.body.id}`);
+    const getResp = await request(app).get(`/api/v1/reviews${review.body.id}`);
     expect(getResp.status).toBe(404);
   });
   afterAll(() => {
